7ffab2b003b1f6342c710fb8522302ec16eeb3f7
Tahama Saleem <tahama.saleem@gmail.com>
2025-09-21 17:20:39 +0500

    Initial commit

---
+++ Switchcase.cpp.cpp
@@ -0,0 +1,65 @@
+ #include <iostream>
+ using namespace std;
+ 
+  
+  int main (){
+  	
+  	int day;
+  	 
+  	 cout << "Enter day number (1-7): ";
+  	 
+  	 cin >> day;
+  	  
+  	  switch (day){
+  	  	
+  	  	case 1:
+  	  		cout <<"MONDAY";
+			break;
+			
+				case 2:
+  	  		cout <<"TUESDAY";
+			break;
+			
+				case 3:
+  	  		cout <<"WEDNESDAY";
+			break;
+			
+				case 4:
+  	  		cout <<"THURSDAY";
+			break;
+			
+				case 5:
+  	  		cout <<"FRIDAY";
+			break;
+			
+				case 6:
+  	  		cout <<"SATURDAY";
+			break;
+			 
+			 	case 7:
+  	  		cout <<"SUNDAY";
+			break;
+			 
+			 deafult:
+			 	cout<< "INVALID DAY/NUMBER.. ";
+			 
+			 }
+			 
+			 
+			 }
+			 	
+			 
+			 
+			 
+
+			 
+			 
+			 
+			 
+  	  	
+		
+  	  
+  	  
+  	  
+  
+ 
